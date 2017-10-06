7e84d6c9596e846aae4b107c0bdc9e17edc922a6
david morse <davidbryanmarshall@gmail.com>
2017-10-05 23:02:37 -0700

    scripting and layout except for math completed, plus README info

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# train-scheduler
+# Train Scheduler
+
+Week Four Homework Part 1: [Firebase Assignment - Basic](http://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/09-11-2017-UCB-Class-Repository-FSF-FT/blob/master/04-week/homework/part-1/Instructions/Homework_Train_Activity_Basic.md).
+
+### No Prerequisites, Installation, or Testing Instructions required
+
+## Misc Notes
+
+* Among game enhancements beyond [mockup image](hhttp://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/09-11-2017-UCB-Class-Repository-FSF-FT/blob/master/04-week/homework/part-1/Instructions/Train_Time_Image.png):
+  * Current Schedule row doesn't appear until data exists.
+  * Remove button included.  
+
+## Author
+
+* **David Morse** ([dbmarshall.github.io](https://dbmarshall.github.io))
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,112 +18,91 @@ $(document).ready(function() {
   var database = firebase.database();
 
   // Initial Values
-  var name = '';
-  var role = '';
-  var start = '';
-  var rate = '';
-  var worked = '';
-  var billed = '';
+  var trainName = '';
+  var destination = '';
+  var trainTime = '';
+  var frequency = '';
+  var nextArrival = '';
+  var minutesAway = '';
 
   database.ref().on('child_added', function(snapshot) {
 
-      // console.log(snapshot.val().name);
+      // console.log('trainName: ' + snapshot.val().trainName);
+
+      $('#current-schedule-row').show();
 
       var newRow = $('<tr>');
-      name = snapshot.val().name;
-      var newName = $('<td>').text(name);
-      role = snapshot.val().role;
-      var newRole = $('<td>').text(role);
-      start = snapshot.val().start;
-      var newStart = $('<td>').text(start);
-      
-      worked = moment([]).diff(start, 'months');
-      var newWorked = $('<td>').text(worked);
 
-      rate = snapshot.val().rate;
-      var newRate = $('<td>').text(rate);
+      trainName = snapshot.val().trainName;
+      var newName = $('<td>').text(trainName);
 
-      billed = worked * rate;
-      var newBilled = $('<td>').text('$' + billed);
+      destination = snapshot.val().destination;
+      var newDestination = $('<td>').text(destination);
 
-      newRow.append(newName,newRole,newStart,newWorked,newRate,newBilled);
+      frequency = snapshot.val().frequency;
+      var newFrequency = $('<td>').text(frequency);
 
-      $('#employee-data tbody').append(newRow);
+      nextArrival = snapshot.val().nextArrival;
+      var newArrival = $('<td>').text(nextArrival);
+
+      minutesAway = snapshot.val().minutesAway;
+      var newMinutes = $('<td>').text(minutesAway);
+
+      newRow.append(newName,newDestination,newFrequency,newArrival,newMinutes);
+      $('#train-data tbody').append(newRow);
 
   });
 
-  // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-
-  //     // console.log(snapshot.val().name);
-
-  //     // $("#name-display").text(snapshot.val().name);
-  //     // $("#email-display").text(snapshot.val().email);
-  //     // $("#age-display").text(snapshot.val().age);
-  //     // $("#comment-display").text(snapshot.val().comment);
-
-  //     var newRow = $('<tr>');
-  //     name = snapshot.val().name;
-  //     var newName = $('<td>').text(name);
-  //     role = snapshot.val().role;
-  //     var newRole = $('<td>').text(role);
-  //     start = snapshot.val().start;
-  //     var newStart = $('<td>').text(start);
-  //     rate = snapshot.val().rate;
-  //     var newRate = $('<td>').text(rate);
-
-  //     newRow.append(newName,newRole,newStart,newRate);
-
-  //     $('#employee-data tbody').append(newRow);
-
-  // });
-
-  $('#search-btn').on('click', function() {
+  $('#submit-btn').on('click', function() {
 
     event.preventDefault();
 
-    // Grabbed values from text boxes
-    name = $('#employee-name').val().trim();
-    role = $('#role').val().trim();
-    start = $('#start-date').val().trim();
-    rate = $('#monthly-rate').val().trim();
+    // Get values from text boxes
+    trainName = $('#train-name').val().trim();
+    destination = $('#destination').val().trim();
+    trainTime = $('#train-time').val().trim();
+    frequency = $('#frequency').val().trim();
 
-    // Code for handling the push
+    console.log(trainTime);
+
+    // Push to firebase
     database.ref().push(
     {
-      name: name,
-      role: role,
-      start: start,
-      rate: rate,
+      trainName: trainName,
+      destination: destination,
+      trainTime: trainTime,
+      frequency: frequency,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
       // https://www.unixtimestamp.com/
     });
 
-    $('#employee-name').val('');
-    $('#role').val('');
-    $('#start-date').val('');
-    $('#monthly-rate').val('');
+    // Empty form input values
+    $('#train-name').val('');
+    $('#destination').val('');
+    $('#train-time').val('');
+    $('#frequency').val('');
 
   });
 
-  $('#delete-btn').on('click', function() {
+  // $('#delete-btn').on('click', function() {
 
-    event.preventDefault();
+  //   event.preventDefault();
 
-    // Code for handling the push
-    firebase.child('articlesList').orderByChild('site').equalTo('SciShow').once('child_added', function(snapshot){
-        snapshot.ref().remove();  
-      });
-    // database.ref().orderByChild("dateAdded").limitToLast(1).once("child_added", function(snapshot)
-    database.ref().remove(
-    {
-      name: name,
-      role: role,
-      start: start,
-      rate: rate,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-      // https://www.unixtimestamp.com/
-    });
+  //   // Remove from firebase
+  //   firebase.child('articlesList').orderByChild('site').equalTo('SciShow').once('child_added', function(snapshot){
+  //       snapshot.ref().remove();  
+  //     });
+  //   // database.ref().orderByChild("dateAdded").limitToLast(1).once("child_added", function(snapshot)
+  //   database.ref().remove(
+  //   {
+  //     trainName: trainName,
+  //     destination: destination,
+  //     trainTime: trainTime,
+  //     frequency: frequency,
+  //     dateAdded: firebase.database.ServerValue.TIMESTAMP
+  //     // https://www.unixtimestamp.com/
+  //   });
 
-  })
+  // });
   
 });
